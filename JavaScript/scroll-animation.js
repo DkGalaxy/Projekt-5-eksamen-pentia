e54b8f77f2f956462f39fa35a8df28cfd87da95d
sogonolo <signe.kallestrup@gmail.com>
2024-05-29 07:44:02 +0200

    scroll update

--- JavaScript/scroll-animation.js
+++ JavaScript/scroll-animation.js
@@ -1,7 +1,6 @@
 // Scroll animation
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
