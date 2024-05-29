6ec7ad756800702a81199ff5b7b0984759c1cacc
sogonolo <signe.kallestrup@gmail.com>
2024-05-29 08:05:46 +0200

    opdateret scroll

--- JavaScript/scroll-animation.js
+++ JavaScript/scroll-animation.js
@@ -1,4 +1,4 @@
-// Scroll animation
+// Scroll animation  (kilde: https://www.youtube.com/watch?v=T33NN_pPeNI&list=PLAI-Y0y6XsV_YGCcQ1-3uIwNprIbAYZJq&index=17)
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
